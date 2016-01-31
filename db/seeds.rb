6ad7a43b44863906554730cd9fe5db1801269b58
pavelperkin <perkinpv@gmail.com>
2016-01-31 12:57:43 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create([{ title: 'Task-manager' }])
+Project.create title: 'Task-manager'
