12d14a613bde973b6c623ff88b1e3b0b5b5a4399
pavelperkin <perkinpv@gmail.com>
2016-01-31 16:26:45 +0200

    cleanup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
-
     render json: @tasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
     render json: @task
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
-
     @task = Task.new(task_params)
-
     if @task.save
       render json: @task, status: :created, location: @task
     else
@@ -28,11 +19,8 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     @task = Task.find(params[:id])
-
     if @task.update(task_params)
       head :no_content
     else
@@ -40,11 +28,8 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
-
     head :no_content
   end
 
--- app/views/tasks/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @task, :id, :title, :body, :project_id, :priority, :deadline, :done, :created_at, :updated_at
